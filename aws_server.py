bf8ca45816080bfddf711ef85f9793e53c67e338
ekaksher <ekaksher@live.in>
2022-02-07 20:52:10 +0530

    Initial Commit

---
+++ aws_server.py
@@ -0,0 +1,96 @@
+from typing import Any, Callable, Dict, List, Optional, Tuple
+
+import flwr as fl
+import tensorflow as tf
+class AggregateCustomMetricStrategy(fl.server.strategy.FedAvg):
+    def aggregate_evaluate(
+        self,
+        rnd: int,
+        results,
+        failures: List[BaseException],
+    ) -> Optional[float]:
+        """Aggregate evaluation losses using weighted average."""
+        if not results:
+            return None
+
+        # Weigh accuracy of each client by number of examples used
+        accuracies = [r.metrics["accuracy"] * r.num_examples for _, r in results]
+        examples = [r.num_examples for _, r in results]
+
+        # Aggregate and print custom metric
+        accuracy_aggregated = sum(accuracies) / sum(examples)
+        print(f"Round {rnd} accuracy aggregated from client results: {accuracy_aggregated}")
+
+        # Call aggregate_evaluate from base class (FedAvg)
+        return super().aggregate_evaluate(rnd, results, failures)
+def main() -> None:
+    # Load and compile model for
+    # 1. server-side parameter initialization
+    # 2. server-side parameter evaluation
+    model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+    model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    # Create strategy
+    strategy = AggregateCustomMetricStrategy(
+        fraction_fit=0.5,
+        fraction_eval=0.2,
+        min_fit_clients=2,
+        min_eval_clients=1,
+        min_available_clients=2,
+        eval_fn=get_eval_fn(model),
+        on_fit_config_fn=fit_config,
+        on_evaluate_config_fn=evaluate_config,
+        initial_parameters=fl.common.weights_to_parameters(model.get_weights()),
+    )
+
+    # Start Flower server for four rounds of federated learning
+    fl.server.start_server("[::]:8080", config={"num_rounds": 3}, strategy=strategy)
+
+
+def get_eval_fn(model):
+    """Return an evaluation function for server-side evaluation."""
+
+    # Load data and model here to avoid the overhead of doing it in `evaluate` itself
+    (x_train, y_train), _ = tf.keras.datasets.cifar10.load_data()
+
+    # Use the last 5k training examples as a validation set
+    x_val, y_val = x_train[45000:50000], y_train[45000:50000]
+
+    # The `evaluate` function will be called after every round
+    def evaluate(
+        weights: fl.common.Weights,
+    ) -> Optional[Tuple[float, Dict[str, fl.common.Scalar]]]:
+        model.set_weights(weights)  # Update model with the latest parameters
+        loss, accuracy = model.evaluate(x_val, y_val)
+        return loss, {"accuracy": accuracy}
+
+    return evaluate
+
+
+def fit_config(rnd: int):
+    """Return training configuration dict for each round.
+
+    Keep batch size fixed at 32, perform two rounds of training with one
+    local epoch, increase to two local epochs afterwards.
+    """
+    config = {
+        "batch_size": 32,
+        "local_epochs": 1 if rnd < 2 else 2,
+        "epoch":4,
+    }
+    return config
+
+
+def evaluate_config(rnd: int):
+    """Return evaluation configuration dict for each round.
+
+    Perform five local evaluation steps on each client (i.e., use five
+    batches) during rounds one to three, then increase to ten local
+    evaluation steps.
+    """
+    val_steps = 5 if rnd < 4 else 10
+    return {"val_steps": val_steps}
+
+
+if __name__ == "__main__":
+    main()
---
+++ client_basic.py
@@ -0,0 +1,28 @@
+import os
+import math
+
+# Make TensorFlow logs less verbose
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import flwr as fl
+import tensorflow as tf
+
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+
+class CifarClient(fl.client.NumPyClient):
+    def get_parameters(self):
+        return model.get_weights()
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=config['epoch'], batch_size=config['batch_size'],steps_per_epoch=10)
+        return model.get_weights(), len(x_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        return loss, len(x_test), {"accuracy": accuracy}
+
+fl.client.start_numpy_client("[::]:8080", client=CifarClient())
